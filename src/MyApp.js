de0e9105cb29cc5008579dcf537233d20774f8e6
Jaroslav <jaroslav.siroic.com>
2019-10-20 11:03:20 +0200

    Lab2Ex2

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,9 +1,14 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import AppTitle from "./AppTitle";
 import Students from "./Students";
 
 const generateArray = n =>
     [...new Array(n)].map(() => Math.round(Math.random() * 100));
+
+const generateArray2 = (a, b) => {
+    if (a < 0 || a > b) return [];
+    return [...new Array(b - a + 1)].map((_, index) => a + index);
+};
 
 const generateArrayExample = generateArray(10);
 
@@ -21,11 +26,11 @@ const makeSquare = array => array.map(v => Math.pow(v, 2));
 const squares = makeSquare(otherArray);
 
 const App = () => {
-
     const [aValue, setA] = useState(0);
     const [bValue, setB] = useState(0);
+    const [stateArray, setArray] = useState([]);
 
-    console.log("Value change -> a:"+ aValue + " b:"+ bValue);
+    console.log("Value change -> a:" + aValue + " b:" + bValue);
     return (
         <div>
             <h1>Minimal React by Jaroslav Siroic</h1>
@@ -53,10 +58,26 @@ const App = () => {
             <hr />
             <h2>Lab 2</h2>
             <label>A:</label>
-            <input type="number" onChange={(event) => setA(event.target.value)} />
+            <input
+                type="number"
+                onChange={event => {
+                    const change = Number(event.target.value);
+                    setA(change);
+                    setArray(generateArray2(change, bValue));
+                }}
+            />
             <label>B:</label>
-            <input type="number" onChange={(event) => setB(event.target.value)} />
-
+            <input
+                type="number"
+                onChange={event => {
+                    const change = Number(event.target.value);
+                    setB(change);
+                    setArray(generateArray2(aValue, change));
+                }}
+            />
+            {stateArray.map(item => (
+                <span style={{ padding: "10px" }}>{item}</span>
+            ))}
             <footer style={{ height: "500px" }}></footer>
         </div>
     );
